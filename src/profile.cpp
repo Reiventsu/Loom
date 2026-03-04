e690731b3eea629f685646d007ae205c6a9e26fc
Ophelia Nord <lucanord@playgroundsquad.com>
2026-03-04 16:32:25 +0100

    Profiling stuff

---
+++ src/profile.cpp
@@ -0,0 +1,11 @@
+#include "profile.hpp"
+
+namespace loom {
+
+    const sProfile sProfile::Debug          = { "debug" };
+    const sProfile sProfile::RelWithDebInfo = { "relwithdebinfo" };
+    const sProfile sProfile::Release        = { "release" };
+    const sProfile sProfile::MinSizeRel     = { "minsizerel" };
+    const sProfile sProfile::Final          = { "final" };
+
+}
